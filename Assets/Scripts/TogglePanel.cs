fc0e33c3addce855f5c2479a5302f5bbf756442c
Chris DeLeon <gamkedo@gmail.com>
2015-11-03 23:30:32 -0800

    Credits added
    on title screen with a toggle button

---
+++ Assets/Scripts/TogglePanel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TogglePanel : MonoBehaviour {
+	public GameObject whichPanel;
+	public void Toggle() {
+		whichPanel.SetActive( !whichPanel.activeSelf );
+	}
+}
