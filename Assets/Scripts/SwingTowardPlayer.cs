eaa395284f74c61dff9915cd5915e4e49aeaee48
Chris DeLeon <gamkedo@gmail.com>
2015-11-05 00:53:32 -0800

    Title Screen improved and Sasha logo added
    Also minor tweaks to level select screen wording and layout

--- Assets/Scripts/SwingTowardPlayer.cs
+++ Assets/Scripts/SwingTowardPlayer.cs
@@ -3,21 +3,26 @@ using System.Collections;
 
 public class SwingTowardPlayer : MonoBehaviour {
 	public bool noMissing = false;
+	public float ampEffect = 1.0f;
+	public float timeStretch = 1.0f;
+	public bool skipPlayerTest = false;
 
 	// Use this for initialization
 	void Start () {
-		if(PlayerControl.instance == null) {
+		if(skipPlayerTest == false && PlayerControl.instance == null) {
 			Destroy(this); // remove component, no player in scene, must be level menu
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt(PlayerControl.instance.transform.position); // snap tracking
+		if(PlayerControl.instance) {
+			transform.LookAt(PlayerControl.instance.transform.position); // snap tracking
+		}
 		if(noMissing == false) {
 			transform.Rotate( // oscillate swivel, good effect for laser attack
-			Mathf.Cos(Time.time*7.673f) * 2.0f,
-			Mathf.Cos(Time.time*3.1f) * 3.0f, 0.0f, Space.Self);
+			                 Mathf.Cos(Time.time*7.673f * timeStretch) * 2.0f* ampEffect,
+			                 Mathf.Cos(Time.time*3.1f * timeStretch) * 3.0f* ampEffect, 0.0f, Space.Self);
 		}
 	}
 }
